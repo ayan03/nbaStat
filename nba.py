53a4b7e129467113253cb408316047168d57b92f
ckkato <ckato005@gmail.com>
2018-06-26 14:11:37 -0700

    Add files via upload

--- nba.py
+++ nba.py
@@ -6,23 +6,26 @@ from bs4 import BeautifulSoup
 import matplotlib
 
 
-
-res = requests.get('https://basketball.realgm.com/nba/players')
-soup = BeautifulSoup(res.content, 'lxml')
-table = soup.find_all('table')[0]
-df = pd.read_html(str(table))[0]
-
-#creates a list of player names
-players = df["Player"].tolist()
-
-#creates a list of player ages
-ages = df["Age"].tolist()
-
-#creates a list of player positions
-positions = df["Pos"].tolist()
-
 #creates a list of players with respective age and positions
-basic_player_dataset = list(zip(players, ages, positions))
+#takes in (string) url 
+#returns a list of ("name", age, "position")
+def create_basic_dataset(url):
+	res = requests.get(url)
+	soup = BeautifulSoup(res.content, 'lxml')
+	table = soup.find_all('table')[0]
+	df = pd.read_html(str(table))[0]
 
+	#creates a list of player names
+	players = df["Player"].tolist()
 
-print(basic_player_dataset)
+	#creates a list of player ages
+	ages = df["Age"].tolist()
+
+	#creates a list of player positions
+	positions = df["Pos"].tolist()
+
+	basic_player_dataset = list(zip(players, ages, positions))
+
+	return basic_player_dataset
+
+print(create_basic_dataset('https://basketball.realgm.com/nba/players'))
